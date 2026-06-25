f136c419be2f54f382078c3b4cb3669ab0ce9217
Mr-Dhanush-git <dhanuschinthalapudih@gmail.com>
2026-06-25 20:38:07 +0530

    Day 1: Java Basics - Hello World

---
+++ Test.java
@@ -0,0 +1,77 @@
+import java.util.*;
+// public class Test{
+//     public static void main(String[]args){
+//       System.out.println("Monkey D. Luffy");
+//     }
+// }
+
+
+//public class Test{
+//     public static void main(String[]args){
+      
+//         for(int i = 0; i < 5; i++){
+//             for(int j =0; j <i+1 ; j++){
+//                 System.out.print("*");
+//             }
+//             System.out.println();
+//         }
+//     }
+// }
+
+// public class Test{
+//     public static void main(String[]args){
+//         List<Integer> list = new ArrayList<>();
+//         list.add(1);
+//         list.add(2);
+//         list.add(3);
+
+//         String[] arr1 = {"a", "b", "c"};
+//         System.out.println(list);
+//         int sum = 0;
+//         for(int i = 0; i<list.size(); i++){
+//             sum += list.get(i);
+//         }
+//         System.out.println("Sum: " + sum);
+
+//         for(int i=0 ; i<arr1.length ; i++){
+//             System.out.println(arr1[i]);
+//         }
+//         String a = "";
+//         for(int i =0;i<arr1.length;i++){
+//             a+=arr1[i].charAt( 0);
+//         }
+//         System.out.println("Sum of char: " + a);
+//     }
+// }
+
+// public class Test{
+//     public static void main(String[]args){
+//         Map<String, Integer> map = new HashMap<>();
+//         map.put("Dhanush",10);
+//         map.put("luffy", 5);
+//         map.put("ichigo", 30);
+//         map.put("naruto", 4);
+
+//         for(Map.Entry<String, Integer> entry : map.entrySet()){
+//             System.out.println(entry.getKey() + "---> " + entry.getValue());
+//         }
+//         Optional<Integer> marksOfGoku = Optional.ofNullable(map.get("goku"));
+//         if(marksOfGoku.isPresent()){
+//             System.out.println("Marks of Goku: " + marksOfGoku.get());
+//         }else{
+            
+//             System.out.println("Goku graduated from the school");
+//         }
+//     }
+// }
+
+// public class Test{
+//     public static void main(String[]args){
+//         Set<Integer> set = new HashSet<>();
+//         set.add(28);
+//         set.add(6);
+//         set.add(100);
+//         System.out.println(set);
+//     }
+// }
+
